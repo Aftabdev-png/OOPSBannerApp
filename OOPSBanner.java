92017cb165e96db08c41af2652c1659fdb748a87
Md Aftabuddin <ma8482@srmist.edu.in>
2026-02-17 08:33:31 +0530

    OOPSBannerApp

---
+++ OOPSBanner.java
@@ -0,0 +1,15 @@
+/**
+*OOPSBannerApp UC1 - OOPS Banner Display Applocation
+*
+*This class demonstrates a simple java application that displays the Object
+*Oriented Programming System OOPS acronym to the console.
+*
+* @author DEveloper
+* @version 1.0
+*/
+
+public class OOPSBanner{
+	public static void main (String[] args){
+		System.out.println ("OOPS");
+	}
+}
